d1ecd47520c5443d962fcdebd78afee9a05148a6
Ondrej Harnusek <Ondrej.Harnusek@ibm.com>
2020-11-12 17:50:27 +0100

    Added hash

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -16,29 +16,32 @@
       </v-btn>
     </form>
     <p v-else>
-      Secret message
+      <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRpYqsQrCWvOswzXupAuFmWJDJ6GzcY_XlCYy3zbocRjYxp5rJcwaaSzfpPnkRXY14YdEivcQp0vX9f/embed?start=true&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
     </p>
   </div>
 </template>
 
 <script>
+const shajs = require('sha.js');
+
 export default {
   name: "Admin",
 
   data: () => {
     return {
       isAuthorized: false,
-      secret: 'nbu123',
+      secret: '389ceca3b42c5183c5a74886469e85233111b505caaded85693d4d9a7e877feb',
       password: '',
     }
   },
 
   methods: {
     submit() {
-      if (this.password !== this.secret) {
+      if (shajs('sha256').update(this.password).digest('hex') !== this.secret) {
         alert('Password not valid!')
       } else {
         this.isAuthorized = true;
+        // window.location.href = 'http://google.com';
       }
     }
   }
